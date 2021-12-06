e4385e67ae09b6201732342ba91661785fa31e82
MarekStolz <stolzm95@gmail.com>
2021-12-06 16:37:19 +0100

    init

--- html/js.js
+++ html/js.js
@@ -1,1 +1,15 @@
-
+let data = [];
+let form = document.getElementById("form");
+form.onsubmit = (e) => {
+    e.preventDefault();
+    let age = document.getElementById("age").value;
+    let books = document.getElementById("book").value;
+    data.push({a:age, b:books});
+    console.log(data);
+    
+    let table = document.createElement("table");
+    table.style.borderCollapse = "collapse";
+    for (let row of data) {
+        console.log(row.a, row.b);
+    }
+}
