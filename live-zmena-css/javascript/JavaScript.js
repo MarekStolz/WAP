54a91d04dddd177558db90b07d4930051a31389b
MarekStolz <stolzm95@gmail.com>
2021-12-30 17:53:33 +0100

    init

---
+++ live-zmena-css/javascript/JavaScript.js
@@ -0,0 +1,21 @@
+let mode = "light";
+let dope = "funky";
+document.getElementById("mode").onclick = () => {
+  mode = mode == "light" ? "dark" : "light";
+  event.target.innerText =
+    mode == "light" ? "na tmavý režim" : "na světlý režim";
+  document.getElementById("link-style").href = "css/" + mode + ".css";
+
+  console.log("nastaveno na " + mode);
+};
+
+document.getElementById("dope").onclick = () => {
+  dope = dope == "funky" ? "blue" : "funky";
+
+  event.target.innerText =
+    dope == "funky" ? "na modrý režim" : "na funky režim";
+
+  document.getElementById("link-style").href = "css/" + dope + ".css";
+
+  console.log("nastaveno na " + dope);
+};
