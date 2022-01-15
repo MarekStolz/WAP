f04a52f91cf49873fff0efe148f74467af5e714b
MarekStolz <stolzm95@gmail.com>
2022-01-15 20:45:28 +0100

    init

--- Hamburger menu/js.js
+++ Hamburger menu/js.js
@@ -2,37 +2,3 @@ let url = "http://stolzm.wz.cz";
 document.querySelector(".nadpis").addEventListener("click", function () {
   window.open(url, "_self");
 });
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs((slideIndex += n));
-}
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mike");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
-}
