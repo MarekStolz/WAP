9cab1e4fd442043b65af478027dbeec1f4d84db0
MarekStolz <stolzm95@gmail.com>
2022-01-17 20:25:32 +0100

    init

---
+++ Hamburger menu/JS/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(710, 400);
+  
+    background(0);
+  }
+  
+  function draw() {
+    stroke(255);
+    if (mouseIsPressed === true) {
+      line(mouseX, mouseY, pmouseX, pmouseY);
+    }
+  }
+  
