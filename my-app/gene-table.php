a158fd5e0b1e0b9e15235d9058531ad31e17dc09
Marek Stolz <81717582+MarekStolz@users.noreply.github.com>
2022-03-07 09:30:50 +0100

    Add files via upload

---
+++ my-app/gene-table.php
@@ -0,0 +1,15 @@
+
+                <?php
+                echo"<table>";
+                for ($y = 0; $y < $size; $y++) {
+                    echo "<tr>";
+                    for ($x = 0; $x < $size; $x++) {
+                        echo "<td>";
+
+                        echo "L</td>";
+                    }
+                    echo "</tr>";
+                }
+                echo"</table>";
+                
+           
---
+++ my-app/php.php
@@ -0,0 +1,37 @@
+<?php
+require "init.php";
+?>
+<html>
+
+<head>
+    <title>Karel</title>
+    <meta charset="UTF-8" />
+    <meta name="author" content="Marek Štolz">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <main>
+        <form>
+            <h1>Karel</h1>
+            <fieldset>
+                <legend>Příkazy</legend>
+                <textarea rows="10">reset</textarea>
+                <button>Proveď</button>
+            </fieldset>
+        </form>
+        <div>
+            <?php
+            require "gene-table.php";
+            ?>
+        </div>
+
+        <p>Manual</p>
+
+
+        </content>
+    </main>
+
+    <body>
+        <html>
