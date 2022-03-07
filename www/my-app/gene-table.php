c1d322d343fcead59440185ae70b1d61b4d57972
Marek Stolz <81717582+MarekStolz@users.noreply.github.com>
2022-03-07 09:45:35 +0100

    Add files via upload

---
+++ www/my-app/gene-table.php
@@ -0,0 +1,15 @@
+
+                <?php
+                echo"<table>";
+                for ($y = 0; $y < $size; $y++) {
+                    echo "<tr>";
+                    for ($x = 0; $x < $size; $x++) {
+                        echo "<td>";
+
+                        echo "</td>";
+                    }
+                    echo "</tr>";
+                }
+                echo"</table>";
+                
+           
