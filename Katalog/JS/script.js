0a78305e99105e7dc434542f2ac9948f3c147741
Marek Stolz <81717582+MarekStolz@users.noreply.github.com>
2022-03-22 19:53:49 +0100

    Add files via upload

--- Katalog/JS/script.js
+++ Katalog/JS/script.js
@@ -2,10 +2,12 @@ const gallery = document.getElementById("gallery");
 for(let index in data ){
   let item = data[index];
   let ele = document.createElement('div');
-  ele.innerHTML = `<h2>${item.name}</h2>
+  ele.innerHTML = `
+  <img src="${item.images}" class="img" > 
+  <h2>${item.name}</h2>
   <p>${item.description}</p>
-  <p>${item.autor}</p>
   <p>${item.price}</p>
+  <h3>${item.autor}</h3>
   `;
 
   gallery.appendChild(ele);
