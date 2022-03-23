440f5419a39f2ac8bc9fd1fff4417bb98508a7f0
Marek Stolz <81717582+MarekStolz@users.noreply.github.com>
2022-03-23 10:43:48 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,44 @@
+const data = {
+    p1: {
+      name: "Louže",
+      description: "Muj obraz",
+      autor: "Marek Štolz",
+      price: "1 500 000Kč ",
+      images: "/znak.png",
+    },
+    p2: {
+      name: "Mona Lisa",
+      description: "Obraázek Mony Lisi",
+      autor: "Davinchi",
+      price: "Unlimited",
+      images: "/mona.png",
+    },
+    p3: {
+      name: "Matej ",
+      description: "esaster egg",
+      autor: "Ondřej Jirčák",
+      price: "Nemuže být prodán",
+      images: "/zalud.jpg",
+    },
+    p4: {
+      name: "nazev",
+      description: "popis",
+      autor: "autor",
+      price: "1500",
+      images: "/znak.png",
+    },
+    p5: {
+      name: "nazev",
+      description: "popis",
+      autor: "autor",
+      price: "1500",
+      images: "/znak.png",
+    },
+    p6: {
+      name: "nazev",
+      description: "popis",
+      autor: "autor",
+      price: "1500",
+      images: "/znak.png",
+    },
+  };
---
+++ script.js
@@ -0,0 +1,14 @@
+const gallery = document.getElementById("gallery");
+for (let index in data) {
+  let item = data[index];
+  let ele = document.createElement("div");
+  ele.innerHTML = `
+  <img src="${item.images}" class="img" alt="${item.description}"> 
+  <h2>${item.name}</h2>
+  <p>${item.description}</p>
+  <p>${item.price}</p>
+  <h3>${item.autor}</h3>
+  `;
+
+  gallery.appendChild(ele);
+}
