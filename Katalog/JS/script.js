0da38e2050fe025d5a35337649a4be6d904af630
MarekStolz <stolzm95@gmail.com>
2022-03-23 19:43:47 +0100

    init

--- Katalog/JS/script.js
+++ Katalog/JS/script.js
@@ -1,4 +1,5 @@
 const gallery = document.getElementById("gallery");
+const detail = document.getElementById("detail");
 for (let index in data) {
   let item = data[index];
   let ele = document.createElement("div");
@@ -8,7 +9,19 @@ for (let index in data) {
   <p>${item.description}</p>
   <p>${item.price}</p>
   <h3>${item.autor}</h3>
-  `;
+  `; gallery.appendChild(ele);
 
-  gallery.appendChild(ele);
+  ele.onclick = () => {
+    detail.innerHTML = `<div class="detail-item">
+    <img src="${item.images}" class="img" alt="${item.description}"> 
+  <h2>${item.name}</h2>
+  <p>${item.description}</p>
+  <p>${item.price}</p>
+  <h3>${item.autor}</h3>
+    </div>`;
+        detail.style.display = "flex";
+};
+detail.onclick = () => {
+    detail.style.display = "none";
+}
 }
